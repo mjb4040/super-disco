44a5ad9f8ebbcb741f7edb4ad78eb12938939cb8
mjb4040 <michaeljamesblackwell.@gmail.com>
2021-09-25 12:18:48 -0400

    colors on time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,35 @@ var rightNow = moment().format("MMMM Do, YYYY - hh:mm a");
 console.log(rightNow);
 document.getElementById("currentDay").innerHTML = rightNow;
 
+$(document).ready(function() {
+
+    var textAreas = $(".description")
+    var hour = moment().hour();
+    console.log(textAreas)
+    for (var i = 0; i < textAreas.length; i++) {
+        var currentTextArea = textAreas[i]
+        var currentId = parseInt(currentTextArea.id)
+        //if the current hour is LESS THAN the hour represented by the time block, then that time block is FUTURE
+        // else if the current hour is greater than the time block then that time block in the Past 
+        // else if neither are ture then the time block is in the present 
+        if (hour < currentId) {
+            $(currentTextArea).addClass("future");
+        }
+        else if (hour > currentId) {
+            $(currentTextArea).addClass("past");
+        }
+        else {
+            $(currentTextArea).addClass("present");
+        }
+    }
+})
+
+
+// when page is refreshed, save event is remembered 
+// localStorage.setItem(time, value);
+
+
 //function currentDay (){ 
 //var rightNow = document.getElementById("currentDay");
 //return rightNow;
-// }
-
+//
